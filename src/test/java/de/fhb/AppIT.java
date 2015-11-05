4573a3938d47a08778bc894839d9c1d207b13342
mlevental <m.levental@web.de>
2015-11-05 13:51:20 +0100

    first integration test added

---
+++ src/test/java/de/fhb/AppIT.java
@@ -0,0 +1,19 @@
+package de.fhb;
+
+import junit.framework.TestCase;
+
+public class AppIT extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void firstIT() {
+		assertTrue(true);
+	}
+
+}
