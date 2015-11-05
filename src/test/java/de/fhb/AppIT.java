4be2b351174c8b3df3f32a1f0fc4e0c2aa557417
mlevental <m.levental@web.de>
2015-11-05 14:58:58 +0100

    Integration Test added

--- src/test/java/de/fhb/AppIT.java
+++ src/test/java/de/fhb/AppIT.java
@@ -1,15 +1,13 @@
 package de.fhb;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class AppIT extends TestCase {
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
 	}
 
 	public void firstIT() {
